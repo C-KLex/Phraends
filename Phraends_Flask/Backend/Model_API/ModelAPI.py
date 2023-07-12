dbd01f410f1c4ea17b749a3c765b411917f8d9eb
YLee2121 <kylelee@gapp.nthu.edu.tw>
2023-07-12 16:30:05 -0400

    fix: content missing due to path name change in PR #3

---
+++ Phraends_Flask/Backend/Model_API/ModelAPI.py
@@ -0,0 +1,58 @@
+from transformers import pipeline
+import requests
+import re
+from bs4 import BeautifulSoup
+
+
+def retrieve_article_text(url):
+
+    # Sending an HTTP GET request to the URL
+    response = requests.get(url)
+
+    # Extracting the HTML content from the response
+    html_text = response.text
+
+    # Parsing the HTML using BeautifulSoup
+    soup = BeautifulSoup(html_text, "html.parser")
+
+    # Finding the specific div element that contains the article content, which is the syntax for cnn website
+    article_element = soup.find("div", class_="article__content")
+    article_text = article_element.get_text(
+        strip=True) if article_element else ""
+    return article_text
+
+
+def generate_summarized_sentences(article_text):
+
+    # Creating a summarization pipeline using the BART model
+    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+
+    # Generating a summarized output using the pipeline
+    summarized_output = summarizer(
+        article_text, max_length=130, min_length=30, do_sample=False)
+    summarized_text = summarized_output[0]["summary_text"].strip(
+    ) if summarized_output else ""
+
+    # Splitting the summarized text into individual sentences using regular expressions
+    sentences = re.split(
+        r"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s", summarized_text)
+    return sentences
+
+
+def main():
+    url = "https://www.cnn.com/2023/07/06/tech/twitter-meta-threads-legal-threat/index.html"
+
+    # Retrieve the article
+    article_text = retrieve_article_text(url)
+
+    # Generate summarized sentences
+    summarized_sentences = generate_summarized_sentences(article_text)
+
+    # Print the summarized sentences
+    print("Summarized Sentences:")
+    for i, sentence in enumerate(summarized_sentences, start=1):
+        print(f"{i}. {sentence}")
+
+
+if __name__ == "__main__":
+    main()
