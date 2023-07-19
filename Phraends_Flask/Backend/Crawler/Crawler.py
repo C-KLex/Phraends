f756e60845530e3382ec60e73fc895e7556c817a
Jordan Wen <wjordan@bu.edu>
2023-07-19 15:33:15 -0400

    feat: Added function to obtain 5 articles from dow jones.

--- Phraends_Flask/Backend/Crawler/Crawler.py
+++ Phraends_Flask/Backend/Crawler/Crawler.py
@@ -85,3 +85,58 @@ def get_news_from_investopedia(ticker):
         driver.switch_to.window(driver.window_handles[0])
     driver.quit()
     return links, open("the_news_texts.txt", mode="r")  # .read()
+
+
+def get_news_from_dow_jones(ticker):  # apple
+    driver = webdriver.Chrome()
+    driver.get("https://www.dowjones.com/")
+
+    # Click the search button
+    search_point = driver.find_element(
+        By.XPATH, "/html/body/div[2]/header/div/div[2]/a[1]"
+    )
+    search_point.click()
+
+    # Send in the ticker name
+    search = driver.find_element(
+        By.XPATH, "/html/body/div[6]/div/div/div/div/div/div[3]/form/input"
+    )
+    search.send_keys(str(ticker))
+    search.send_keys(Keys.RETURN)
+
+    # Collect the 5 latest news content
+    links = []
+    driver.implicitly_wait(5)
+    f = open("the_news_texts.txt", "w")
+    for i in range(0, 5):
+        if i == 0:
+            elem = driver.find_element(
+                By.XPATH,
+                "/html/body/div[2]/section[2]/div/div/ul/li[2]/div[1]/div[2]/h3/a",
+            )
+        else:
+            elem = driver.find_element(
+                By.XPATH,
+                f"/html/body/div[2]/section[2]/div/div/ul/li[2]/div[{i+1}]/div[2]/h3/a",
+            )
+        url_element = elem.get_attribute("href")
+        links.append(url_element)
+        # Open the new window
+        driver.execute_script("window.open()")
+        driver.switch_to.window(driver.window_handles[i + 1])
+        driver.get(url_element)
+        time.sleep(1)
+        search_point = driver.find_element(
+            By.XPATH, "/html/body/div[2]/section[2]/div/div/div/div"
+        ).text
+        f.write("This is the %dth article \r\n\n" % (i + 1))
+        f.write(str(search_point))
+        f.write("\n\n\n")
+        # window_handles[0] is a first window
+        driver.switch_to.window(driver.window_handles[0])
+    driver.quit()
+    return links, open("the_news_texts.txt", mode="r")  # .read()
+
+
+print(get_news_from_dow_jones("apple"))
+
