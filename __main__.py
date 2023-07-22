064542816ce1fe5b1c589f8760e60f5a80e9ccaf
Ziqi Lin <ziqilin329@gmail.com>
2023-07-22 16:29:17 -0400

    feat: add drop down menu

--- __main__.py
+++ __main__.py
@@ -8,6 +8,11 @@ st.write("""
 NLP Application for Summarizing Finance Article
  """)
 
+# add the drop down menu
+sp500_df = pd.read_csv('constituents.csv')
+sp500_ls = sp500_df['Symbol'].tolist()
+st.selectbox('Please enter a stock ticker that you would like to learn more about:', sp500_ls)
+
 ticker = st.text_input('Please enter a stock ticker that you would like to learn more about:')
 
 st.write("""1. """, ticker)
@@ -15,8 +20,6 @@ st.write("""2. """, """phrase2""")
 st.write("""3. """, """phrase3""")
 st.write("""4. """, )
 st.write("""5. """, )
-
-
 
 st.write("""
 🔥 Trending Tickers: 
