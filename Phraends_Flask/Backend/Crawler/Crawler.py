770d54b58529fda89b109f6bc8fb522a6cd97628
Ziqi Lin <ziqilin329@gmail.com>
2023-07-31 22:18:06 -0400

    feat: add try except in for loop

--- Phraends_Flask/Backend/Crawler/Crawler.py
+++ Phraends_Flask/Backend/Crawler/Crawler.py
@@ -210,22 +210,24 @@ def get_news_from_cnbc(ticker):
     driver.implicitly_wait(5)
     f = open("the_news_texts.txt", "w")
     for i in range(0, 5):
+        try:
+            elem = driver.find_element(By.XPATH, f'//*[@id="QuotePage-latestNews-0-{i+1}"]/div/div/a')
 
-        elem = driver.find_element(By.XPATH, '//*[@id="QuotePage-latestNews-0-{i+1}"]/div/div/a')
-
-        url_element = elem.get_attribute("href")
-        links.append(url_element)
-        # Open the new window
-        driver.execute_script("window.open()")
-        driver.switch_to.window(driver.window_handles[i + 1])
-        driver.get(url_element)
-        time.sleep(10)
-        search_point = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div[3]/div/div/div/div[3]/div[1]/div[2]').text
-        f.write("This is the %dth article \r\n\n" % (i + 1))
-        f.write(str(search_point))
-        f.write("\n\n\n")
-        # window_handles[0] is a first window
-        driver.switch_to.window(driver.window_handles[0])
+            url_element = elem.get_attribute("href")
+            links.append(url_element)
+            # Open the new window
+            driver.execute_script("window.open()")
+            driver.switch_to.window(driver.window_handles[i + 1])
+            driver.get(url_element)
+            time.sleep(10)
+            search_point = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[1]/div[3]/div/div/div/div[3]/div[1]/div[2]').text
+            f.write("This is the %dth article \r\n\n" % (i + 1))
+            f.write(str(search_point))
+            f.write("\n\n\n")
+            # window_handles[0] is a first window
+            driver.switch_to.window(driver.window_handles[0])
+        except:
+            continue
     driver.quit()
     return (
         links,
