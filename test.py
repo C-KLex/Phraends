26848b683a2df1bc1c83cb37933405aaa9dfc9b0
Alex <alex308248@gmail.com>
2023-09-04 09:33:08 -0400

    modefy test.py and try deploy

--- test.py
+++ test.py
@@ -2,14 +2,30 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
+import streamlit as st
+
 options = Options() 
 options.add_argument("--headless=new")
 options.add_argument('--disable-gpu')
 
 driver = webdriver.Chrome(options=options)
 
-driver.get('https://www3.nohhi.co.jp/rktrace/trace.html')
+url = f"http://www.investopedia.com/search?q=MMM"
+driver.get(url)
 
-search_bar = driver.find_element(By.NAME, "command5")
-search_bar.send_keys(num)
-search_bar.submit()
+links = []
+search_result_block_links = driver.find_elements(By.CSS_SELECTOR, "#search-results__results_1-0 a")
+
+
+for e in search_result_block_links:
+    l = e.get_attribute("href")
+    links.append(str(l))
+        
+try:
+    end_index = links.index("None")
+    links = links[:end_index]
+except:
+    pass 
+
+
+st.write(links)
