92dc42c48187c98d3cdcce8266975c015702ccc4
Hsuan <yhf@bu.edu>
2023-09-11 12:14:14 -0400

    fix: the max_token problem solved

--- Phraends_Pkg/Backend/Model_API/URLBart.py
+++ Phraends_Pkg/Backend/Model_API/URLBart.py
@@ -1,9 +1,20 @@
 from transformers import pipeline
-from Phraends_Pkg.Backend.Crawler import Scrape
 
 def generate_summarized_text(article_text):
     summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
-    summarized_output = summarizer(article_text, max_length=130, min_length=30, do_sample=False)
-    summarized_text = summarized_output[0]["summary_text"].strip() if summarized_output else ""
-    
+
+    # Split the article text into smaller chunks
+    max_chunk_length = 4500 
+    chunks = [article_text[i:i+max_chunk_length] for i in range(0, len(article_text), max_chunk_length)]
+
+    sum_texts = []
+
+    for chunk in chunks:
+        summarized_output = summarizer(chunk, max_length=300, min_length=50, do_sample=False)
+        sum_text = summarized_output[0]["summary_text"].strip() if summarized_output else ""
+        sum_texts.append(sum_text)
+
+    # Combine the summarized chunks into the final summarized text
+    summarized_text = " ".join(sum_texts)
+
     return summarized_text
